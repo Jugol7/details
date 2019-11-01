c34a450a0084f9ff331797c8235ddb4e14423347
zhulinpeng <zhulinpeng@szhtxx.com>
2019-11-01 11:06:46 +0800

    robTicket

--- src/main/java/com/details/robticket/RobTicket.java
+++ src/main/java/com/details/robticket/RobTicket.java
@@ -2,12 +2,15 @@ package com.details.robticket;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 模拟抢票
  * 1.100张票
  * 2.每张票都有编码
- *
+ * 3.
  * @author zlp
  * @date 2019-10-31 17:46
  */
@@ -16,16 +19,28 @@ public class RobTicket {
     /**
      * 拿100不同的张票
      */
-    public void getMap() {
+    public static Map<Integer,TicketInfo> getMap() {
         Map<Integer,TicketInfo> map = new HashMap<>();
         for (int i = 0; i < 100; i++) {
             TicketInfo ticketInfo = new TicketInfo();
-            ticketInfo.setId(i+1);
-            ticketInfo.setName(ticketInfo.getId()+"张票");
             map.put(i,ticketInfo);
         }
+        return map;
     }
 
+    public static void main(String[] args) {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(3,10,200, TimeUnit.SECONDS,new ArrayBlockingQueue<>(5));
+        Map<Integer,TicketInfo> map = getMap();
+        TicketInfo ticketInfo = new TicketInfo();
+        ticketInfo.setRest(100);
+        for(int i = 0; i<map.size(); i++){
+            threadPoolExecutor.execute(ticketInfo);
+            //这直接走一百次，再进run？
+            System.out.print("getPoolSize: "+threadPoolExecutor.getPoolSize());
+            System.out.print("getQueue().size: "+threadPoolExecutor.getQueue().size());
+            System.out.println("getCompletedTaskCount: "+threadPoolExecutor.getCompletedTaskCount());
+        }
+    }
 
 
 
--- src/main/java/com/details/robticket/TicketInfo.java
+++ src/main/java/com/details/robticket/TicketInfo.java
@@ -7,8 +7,25 @@ import lombok.Data;
  * @date 2019-10-31 17:55
  */
 @Data
-public class TicketInfo {
+public class TicketInfo implements Runnable {
 
-    private String name;
-    private Integer id;
+    private int num;
+    private int rest;
+
+    @Override
+    public void run() {
+        //监控的位置，如果放在while里面，那么会出现最后一张票买了十次，因为已经又是个线程进入了while体。
+        synchronized (this) {
+            while (rest > 0) {
+                num++;
+                rest--;
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("当前线程为："+Thread.currentThread().getName() + "----售出第" + num + "张票，还剩下" + rest + "张。");
+            }
+        }
+    }
 }
