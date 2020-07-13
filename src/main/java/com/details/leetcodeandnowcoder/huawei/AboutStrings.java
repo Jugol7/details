efe26949a0ecdef5e14fe821e027c13a5799f023
Jugol7 <971894709@qq.com>
2020-07-13 19:52:38 +0800

    算法题，输出八个字符

--- src/main/java/com/details/leetcodeandnowcoder/huawei/AboutStrings.java
+++ src/main/java/com/details/leetcodeandnowcoder/huawei/AboutStrings.java
@@ -5,6 +5,9 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * 华为笔试算法题--String篇
  *
@@ -19,7 +22,8 @@ public class AboutStrings {
     public static void main(String[] args) {
 
 //        testTheLengthOfLastWord();
-        testFindString();
+//        testFindString();
+        eightArray("");
 
     }
 
@@ -78,11 +82,97 @@ public class AboutStrings {
         }
         for (int i = 0; i < str.length(); i++) {
 //            logger.info("{}",str.charAt(i));
-            if(regex == (str.charAt(i))){
+            if (regex == (str.charAt(i))) {
                 result++;
             }
         }
         return result;
+    }
+
+    /**
+     * 连续输入字符串，请按长度为8拆分每个字符串后输出到新的字符串数组；
+     * 长度不是8整数倍的字符串请在后面补数字0，空字符串不处理
+     *
+     * @param string
+     */
+    public static void eightArray(String string) {
+        Scanner scanner = new Scanner(System.in);
+        //这个输入的数字，长度大8，小8，等8 三种情况，空串另外考虑
+        while (scanner.hasNext()) {
+            String str = scanner.next();
+            if (StringUtils.isBlank(str)) {
+                return;
+            }
+            int len = str.length();
+            if (len == 8) {
+                String[] strings = new String[8];
+                logger.info("长度为8的字符串：{}", str);
+                logger.info("接下来转换成字符串数组：");
+                for (int i = 0; i < len; i++) {
+                    strings[i] = String.valueOf(str.charAt(i));
+                }
+                logger.info("字符串数组如下：{}", Arrays.toString(strings));
+            } else if (len < 8) {
+                logger.info("长度小于8的字符串：{}", str);
+                String[] strings = new String[8];
+                for (int i = 0; i < strings.length; i++) {
+                    if (i + 1 < len) {
+                        strings[i] = String.valueOf(str.charAt(i));
+                    } else {
+                        strings[i] = "0";
+                    }
+                }
+                logger.info("字符串数组如下：{}", Arrays.toString(strings));
+            } else {
+                logger.info("长度大于8的字符串：{}", str);
+                int count = len / 8;
+                //需要count+1个数组
+                for (int i = 0; i < count + 1; i++) {
+                    String[] strings = new String[8];
+                    if (i == count) {
+                        for (int j = 0; j < 8; j++) {
+                            int rest = len%8;
+                            if(rest == 0){
+                                break;
+                            }
+                            if(j < rest){
+                                strings[j] = String.valueOf(str.charAt(i * 8 + j));
+                            }else {
+                                strings[j] = "0";
+                            }
+                        }
+                        logger.info("第 {} 个数组，字符串数组如下：{}", i+1, Arrays.toString(strings));
+                    }else {
+                        for (int j = 0; j < 8; j++) {
+                            strings[j] = String.valueOf(str.charAt(i * 8 + j));
+                        }
+                        logger.info("第 {} 个数组，字符串数组如下：{}", i+1, Arrays.toString(strings));
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * https://www.nowcoder.com/questionTerminal/d9162298cb5a437aad722fccccaae8a7
+     * 牛客网大佬写的代码
+     */
+    public static void eightArrayNiuKe(){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String s = sc.nextLine();
+            if(s.length()%8 != 0){
+                //如果需要补0，直接加8个0
+                s = s+"00000000";
+            }
+            while(s.length() >= 8){
+                //\输出前八个字符
+                System.out.println(s.substring(0,8));
+                //将前八个字符干掉
+                s = s.substring(8);
+            }
+        }
+
     }
 
 
