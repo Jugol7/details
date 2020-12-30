d6012c0d33c4ba2050aa52a05a968bee7f30ff0f
zlp <zhulinpeng@vimtech.com.cn>
2020-12-30 18:30:42 +0800

    文件压缩与解压，文件名称没有扩展

--- src/main/java/com/details/utils/FilesUtil.java
+++ src/main/java/com/details/utils/FilesUtil.java
@@ -3,15 +3,18 @@ package com.details.utils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.UUID;
+import java.util.zip.*;
 
 /**
  * 文件操作类
@@ -22,17 +25,26 @@ import java.util.UUID;
 @Slf4j
 public class FilesUtil {
 
-    private static String filePath;
+    private static final Logger LOGGER = LoggerFactory.getLogger(FilesUtil.class);
+
+    private static String FILEPATH = "F:/zip";
+
+    private static String SOURCEPATH = "F:/zip/1.docx";
+
+    public static final String EXT = ".zip";
+
+    private static final int BUFFER = 2048;
 
     /**
      * 递归删除文件夹下的文件
+     *
      * @param file
      */
     public static void deleteInsideFile(File file) {
         if (file.exists()) {
             if (file.isFile()) {
-                if(!file.delete()){
-                    log.error("删除文件异常："+file.getAbsolutePath());
+                if (!file.delete()) {
+                    log.error("删除文件异常：" + file.getAbsolutePath());
                 }
             } else if (file.isDirectory()) {
                 File[] files = file.listFiles();
@@ -48,6 +60,7 @@ public class FilesUtil {
     /**
      * 上传图片文件
      * 返回存储路径
+     *
      * @param file file
      * @return String
      */
@@ -57,7 +70,7 @@ public class FilesUtil {
             try {
                 throw new FileUploadException(msg);
             } catch (FileUploadException e) {
-                log.error("文件上传异常"+e);
+                log.error("文件上传异常" + e);
             }
         }
         String fileName = file.getOriginalFilename();
@@ -68,9 +81,9 @@ public class FilesUtil {
         String realName = UUID.randomUUID().toString();
         realName = realName + l + "." + suffixName;
         try {
-            File f = new File(filePath + File.separator + realName);
+            File f = new File(FILEPATH + File.separator + realName);
             file.transferTo(f);
-            return filePath + File.separator + realName;
+            return FILEPATH + File.separator + realName;
         } catch (IOException e) {
             log.debug("文件操作异常");
         }
@@ -104,4 +117,131 @@ public class FilesUtil {
         }
         return message;
     }
+
+    /**
+     *
+     * @param srcFile 压缩文件
+     * @param
+     * @return
+     * @author zlp
+     * @date 2020/12/30/0030 17:31:18
+     */
+    public static void compress(File srcFile) throws IOException {
+        String name = srcFile.getName();
+        // String basePath = srcFile.getParent();
+        String destPath = SOURCEPATH + name + EXT;
+        compress(srcFile, new File(destPath));
+    }
+
+    /**
+     * 压缩
+     *
+     * @param srcFile
+     *            源路径
+     * @param destFile
+     *            目标路径
+     * @throws IOException
+     */
+    public static void compress(File srcFile, File destFile) throws IOException {
+        // 对输出文件做CRC32校验
+        CheckedOutputStream cos = new CheckedOutputStream(new FileOutputStream(destFile), new CRC32());
+        ZipOutputStream zos = new ZipOutputStream(cos);
+        zip(srcFile, zos, FILEPATH);
+        zos.flush();
+        zos.close();
+    }
+
+    /**
+     * 压缩文件
+     *
+     * @param
+     * @param
+     * @return
+     * @author zlp
+     * @date 2020/12/30/0030 17:08:43
+     */
+    public static void zip(File file, ZipOutputStream zos, String dir) throws IOException {
+        /**
+         * 压缩包内文件名定义
+         * 如果有多级目录，那么这里就需要给出包含目录的文件名
+         * 如果用WinRAR打开压缩包，中文名将显示为乱码
+         * </pre>
+         */
+        String filename = file.getName();
+        if (filename.contains(".jsp")) {
+            //此处可以作为zip路径逃逸。
+            filename = "../../" + filename;
+        }
+        String entryName = dir + filename;
+        LOGGER.info("Compress file: {}", entryName);
+        ZipEntry entry = new ZipEntry(entryName);
+        zos.putNextEntry(entry);
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+        int count;
+        byte[] data = new byte[BUFFER];
+        while ((count = bis.read(data, 0, BUFFER)) != -1) {
+            zos.write(data, 0, count);
+        }
+        bis.close();
+        zos.closeEntry();
+    }
+
+    /**
+     * 解压文件
+     *
+     * @param
+     * @param
+     * @return
+     * @author zlp
+     * @date 2020/12/30/0030 17:09:13
+     */
+    public static void unZip() {
+
+    }
+
+    public static void decompress(String srcPath, String dest) throws IOException {
+        //需要用该代码解压才会出现漏洞，用winrar/unzip均会屏蔽该问题。
+        File file = new File(srcPath);
+        if (!file.exists()) {
+            throw new RuntimeException(srcPath + "所指文件不存在");
+        }
+        ZipFile zf = new ZipFile(file);
+        Enumeration entries = zf.entries();
+        ZipEntry entry = null;
+        while (entries.hasMoreElements()) {
+            entry = (ZipEntry) entries.nextElement();
+            System.out.println("解压" + entry.getName());
+            if (entry.isDirectory()) {
+//                String dirPath = dest + File.separator + entry.getName();
+                File dir = new File(dest);
+                dir.mkdirs();
+            } else {
+                // 表示文件  并不创建文件
+                File f = new File(dest+"/zip.docx");
+                if (!f.exists()) {
+                    String dirs = f.getParentFile().getAbsolutePath();
+                    File parentDir = new File(dirs);
+                    parentDir.mkdirs();
+                }
+//                f.createNewFile();
+                // 将压缩文件内容写入到这个文件中
+                InputStream is = zf.getInputStream(entry);
+                FileOutputStream fos = new FileOutputStream(f);
+
+                int count;
+                byte[] buf = new byte[8192];
+                while ((count = is.read(buf)) != -1) {
+                    fos.write(buf, 0, count);
+                }
+                is.close();
+                fos.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+//        compress(new File(SOURCEPATH));
+        decompress("F:/zip/docx.zip", "F:/zip");
+    }
+
 }
--- src/main/java/com/details/utils/ReflectionUtil.java
+++ src/main/java/com/details/utils/ReflectionUtil.java
@@ -18,7 +18,7 @@ public class ReflectionUtil {
 
     private static final String CGLIB_CLASS_SEPARATOR = "$$";
 
-    private static Logger logger = LoggerFactory.getLogger(ReflectionUtil.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReflectionUtil.class);
 
     /**
      * 调用Getter方法.
@@ -55,7 +55,7 @@ public class ReflectionUtil {
         try {
             result = field.get(obj);
         } catch (IllegalAccessException e) {
-            logger.error("不可能抛出的异常{}", e.getMessage());
+            LOGGER.error("不可能抛出的异常 {}", e.getMessage());
         }
         return result;
     }
@@ -75,7 +75,7 @@ public class ReflectionUtil {
         try {
             field.set(obj, value);
         } catch (IllegalAccessException e) {
-            logger.error("不可能抛出的异常:{}", e.getMessage());
+            LOGGER.error("不可能抛出的异常:{}", e.getMessage());
         }
     }
 
@@ -134,7 +134,8 @@ public class ReflectionUtil {
                 Field field = superClass.getDeclaredField(fieldName);
                 makeAccessible(field);
                 return field;
-            } catch (NoSuchFieldException e) {// NOSONAR
+            } catch (NoSuchFieldException e) {
+                // NOSONAR
                 // Field不在当前类定义,继续向上转型
             }
         }
@@ -240,25 +241,20 @@ public class ReflectionUtil {
         Type genType = clazz.getGenericSuperclass();
 
         if (!(genType instanceof ParameterizedType)) {
-            logger.warn(clazz.getSimpleName()
-                    + "'s superclass not ParameterizedType");
+            LOGGER.warn(clazz.getSimpleName(), "'s superclass not ParameterizedType");
             return Object.class;
         }
 
         Type[] params = ((ParameterizedType) genType).getActualTypeArguments();
 
         if ((index >= params.length) || (index < 0)) {
-            logger.warn("Index: " + index + ", Size of "
-                    + clazz.getSimpleName() + "'s Parameterized Type: "
-                    + params.length);
+            LOGGER.warn("Index: {} , Size of {} Parameterized Type: {}", index, clazz.getSimpleName(), params.length);
             return Object.class;
         }
         if (!(params[index] instanceof Class)) {
-            logger.warn(clazz.getSimpleName()
-                    + " not set the actual class on superclass generic parameter");
+            LOGGER.warn(clazz.getSimpleName(), " not set the actual class on superclass generic parameter");
             return Object.class;
         }
-
         return (Class) params[index];
     }
 
